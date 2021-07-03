5cc57b24e5c8c9758e6789da34bceb92ae1db4f5
jiye-7 <jy522hi@gmail.com>
2021-07-03 23:36:24 +0900

    Drag and Drop add

--- src/column.jsx
+++ src/column.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
+import { Droppable } from 'react-beautiful-dnd';
 import Task from './task';
 
 const Container = styled.div`
@@ -8,9 +9,11 @@ const Container = styled.div`
   border-radius: 0.3rem;
   background-color: #e3f2fd;
 `;
+
 const Title = styled.h3`
   padding: 0.5rem;
 `;
+
 const TaskList = styled.div`
   padding: 0.5rem;
   background-color: #bbdefb;
@@ -18,16 +21,22 @@ const TaskList = styled.div`
   margin: 0.2rem 0.2rem;
 `;
 
-
 export default class Column extends React.Component {
   render() {
     return (
       <Container>
         <Title>{this.props.column.title}</Title>
-        <TaskList>
-          {this.props.tasks.map(task => <Task key={task.id} task={task}/>)}
-        </TaskList>
+        <Droppable droppableId={this.props.column.id}>
+          {(provided) => (
+            <TaskList ref={provided.innerRef} {...provided.droppableProps}>
+              {this.props.tasks.map((task, index) => (
+                <Task key={task.id} task={task} index={index} />
+              ))}
+              {provided.placeholder}
+            </TaskList>
+          )}
+        </Droppable>
       </Container>
-    )
+    );
   }
 }
