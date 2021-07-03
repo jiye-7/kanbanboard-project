cc3af4489273df506f864c29b91a1a9ae7765b42
jiye-7 <jy522hi@gmail.com>
2021-07-04 00:26:39 +0900

    💄 drag&drop 시 저장되지 않는 문제 해결(onDragEnd 구현)

--- src/index.js
+++ src/index.js
@@ -10,6 +10,48 @@ class App extends React.Component {
 
   onDragEnd = (result) => {
     // TODO: reorder our column
+    const { destination, source, draggableId } = result;
+
+    // 드래그를 했을 때, 결과가 없다면 아무것도 하지 않는다.
+    if (!destination) {
+      return;
+    }
+
+    // 드래그한 위치가 변경되었는 지 확인, 드래그하는 대상 id가 소스와 동일한지? 인덱스가 동일한지 확인
+    // 두 가지가 사실이면 사용자가 항목을 다시 시작된 위치르 끌어다 놓기 때문에 아무것도 하지 않는다.
+    if (
+      destination.droppableId === source.droppableId &&
+      destination.index === source.index
+    ) {
+      return;
+    }
+
+    // 열에 대한 task id 배열의 순서를 다시 지정한다. state에서 column을 읽어와서 드롭 파일 id를 사용해서 state에서의 열을 조회
+    const column = this.state.columns[source.droppableId];
+
+    // 업데이트 할 때 기존 상태의 변형을 방지하고, 변경 사항에 대한 새 객체를 생성하기 위해 마지막 배열의 값과 동일한 내용을 포함하는 것을 가져와서 얕은 복사를 함
+    const newTaskIds = Array.from(column.taskIds);
+    // 이동할 task를 이전 인덱스에서 Array의 새 인덱스로 이동해야 된다. splice: 배열의 기존 요소를 삭제 또는 교체하거나 새 요소를 추가하여 배열의 내용을 변경
+    newTaskIds.splice(source.index, 1);
+    // 드래그 가능한 id를 삽입: 목적지 인덱스, 0개를 지움, 드래그 할 id(task id)
+    newTaskIds.splice(destination.index, 0, draggableId);
+
+    // 새 열 생성, 이 열은 새 작업 id 배열에서 이전 열과 동일한 속성을 가진다. 새로운 칼럼이 생겼으니 state의 새로운 부분으로 본다.
+    // spread syntax를 사용해서 주 객체의 이전 속성을 유지하되, 참조를 무효화
+    const newColumn = {
+      ...column,
+      taskIds: newTaskIds,
+    };
+
+    const newState = {
+      ...this.state,
+      columns: {
+        ...this.state.columns,
+        [newColumn.id]: newColumn,
+      },
+    };
+
+    this.setState(newState);
   };
 
   render() {
